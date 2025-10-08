bea7b521d1b5c787b0796bac7d5cae7818c778b6
akshaymyadapu <myadapuakshay@gmail.com>
2025-10-08 15:15:47 +0530

    oops test

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
-from operations import saving_account,Business_account
-
-
-
-print("1. Saving account")
-print("2. Business account")
-choose=int(input("choose one option: "))
-if choose == 1:
-    print("You have chosen saving account")
-    saving_account()
-elif choose == 2:
-    print("You have chosen business account")
-else:
-    print("Invalid option")
+Class Bank:
+    def __init__(self, name,balance,pin,acno)
+        self.name=name
+        self.balance=0
+        self.__pin=pin
+        self.acno=acno
+    def savingsaccount(self):
+        return f"Account holder name is {self.name} and balance is {self.balance} this is savings account balance"
+    def currentaccount(self):
+        return f"Account holder name is {self.name} and balance is {self.balance} this is current account balance"
+    def checkbalance(self,enteredpin):
+        if enteredpin==self.__pin:
+            return f"Your balance is {self.balance}"
+        else:
+            return "wrong pin"
+    def forzeaccount(self,frozen):
+        if frozen==self.__pin:
+            self.balance>=100000000
+            return "your account is frozen"
+        else:
+            return "your account is active"
+    def deposit(self,amount):
+        self.balance=self.balance+amount
+        return f"your account is credited with {amount} and your new balance is {self.balance}"
+    def withdraw(self,amount,enteredpin):
+        if enteredpin==self.pin:
+            if self.balance=self.balance-amount
+                return f"your account is debited with {amount} and your new balance is {self.balance}"
+            else:
+                amount>self.balance
+                return "insufficient balance"
